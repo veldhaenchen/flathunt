51d098da73241af5bc315b4903d14dbbb44834b7
Arthur Taylor <arthur@codders.de>
2020-05-16 20:25:49 +0200

    Update ImmoScout crawler from another branch (was broken here)

--- flathunter/crawl_immobilienscout.py
+++ flathunter/crawl_immobilienscout.py
@@ -5,35 +5,42 @@ from bs4 import BeautifulSoup
 class CrawlImmobilienscout:
     __log__ = logging.getLogger(__name__)
     URL_PATTERN = re.compile(r'https://www\.immobilienscout24\.de')
+    RESULT_LIMIT = 50
 
     def __init__(self):
         logging.getLogger("requests").setLevel(logging.WARNING)
 
     def get_results(self, search_url):
         # convert to paged URL
-        if '/P-' in search_url:
-            search_url = re.sub(r"/Suche/(.+?)/P-\d+", "/Suche/\1/P-{0}", search_url)
+        # if '/P-' in search_url:
+        #     search_url = re.sub(r"/Suche/(.+?)/P-\d+", "/Suche/\1/P-{0}", search_url)
+        # else:
+        #     search_url = re.sub(r"/Suche/(.+?)/", r"/Suche/\1/P-{0}/", search_url)
+        if '&pagenumber' in search_url:
+            search_url = re.sub(r"&pagenumber=1", "&pagenumber={0}", search_url)
         else:
-            search_url = re.sub(r"/Suche/(.+?)/", r"/Suche/\1/P-{0}/", search_url)
+            search_url = search_url + '?pagenumber={0}'
         self.__log__.debug("Got search URL %s" % search_url)
 
         # load first page to get number of entries
         page_no = 1
         soup = self.get_page(search_url, page_no)
-        no_of_results = int(
-            soup.find_all(lambda e: e.has_attr('data-is24-qa') and e['data-is24-qa'] == 'resultlist-resultCount')[
-                0].text)
-        self.__log__.info('Number of results: ' + str(no_of_results))
-
+        try:
+            no_of_results = int(soup.find_all(lambda e: e.has_attr('data-is24-qa') and e['data-is24-qa'] == 'resultlist-resultCount')[0].text)
+        except IndexError:
+            self.__log__.debug('Index Error occured')
         # get data from first page
         entries = self.extract_data(soup)
 
         # iterate over all remaining pages
-        while len(entries) < no_of_results:
-            self.__log__.debug('Next Page')
+        while len(entries) < min(no_of_results, self.RESULT_LIMIT):
+            self.__log__.debug('Next Page, Number of entries : ' + str(len(entries)) + "no of resulst: " + str(no_of_results))
             page_no += 1
             soup = self.get_page(search_url, page_no)
-            entries.extend(self.extract_data(soup))
+            cur_entry = self.extract_data(soup)
+            if cur_entry == []:
+                break
+            entries.extend(cur_entry)
 
         return entries
 
@@ -44,31 +51,49 @@ class CrawlImmobilienscout:
         return BeautifulSoup(resp.content, 'html.parser')
 
     def extract_data(self, soup):
-        entries = list()
+        entries = []
 
-        title_elements = soup.find_all(lambda e: e.has_attr('class') and 'result-list-entry__brand-title' in e['class'])
-        expose_ids = list([int(e.parent['href'].split('/')[-1].replace('.html', '')) for e in title_elements])
-        expose_urls = list(['https://www.immobilienscout24.de/expose/' + str(ids) for ids in expose_ids])
+        title_elements = soup.find_all(lambda e: e.name == 'a' and e.has_attr('class') and 'result-list-entry__brand-title-container' in e['class'])
+        expose_ids = []
+        expose_urls = []
+        for link in title_elements:
+            expose_id = int(link.get('href').split('/')[-1].replace('.html', ''))
+            expose_ids.append(expose_id)
+            if(len(str(expose_id)) > 5):
+                expose_urls.append('https://www.immobilienscout24.de/expose/' + str(expose_id))
+            else:
+                expose_urls.append(link.get('href'))
+        self.__log__.debug(expose_ids)
+
         attr_container_els = soup.find_all(lambda e: e.has_attr('data-is24-qa') and e['data-is24-qa'] == "attributes")
         address_fields = soup.find_all(lambda e: e.has_attr('class') and 'result-list-entry__address' in e['class'])
-
         for idx, title_el in enumerate(title_elements):
+            attr_els = attr_container_els[idx].find_all('dd')
             try:
-                attr_els = attr_container_els[idx].find_all('dd')
-                address = address_fields[idx].text.strip().replace(',', '')
-                if len(attr_els) > 2:
-                    details = {
-                        'id': expose_ids[idx],
-                        'url': expose_urls[idx],
-                        'title': title_el.text.strip().replace('NEU', ''),
-                        'price': attr_els[0].text.strip().split(' ')[0].strip(),
-                        'size': attr_els[1].text.strip().split(' ')[0].strip() + " qm",
-                        'rooms': attr_els[2].text + " Zi.",
-                        'address': address
-                    }
-                entries.append(details)
+                address = address_fields[idx].text.strip()
             except:
-                print("Error")
+                address = "No address given"
+            if(len(attr_els)>2) :
+                details = {
+                    'id': expose_ids[idx],
+                    'url': expose_urls[idx],
+                    'title': title_el.text.strip().replace('NEU', ''),
+                    'price': attr_els[0].text.strip().split(' ')[0].strip(),
+                    'size': attr_els[1].text.strip().split(' ')[0].strip() + " qm",
+                    'rooms': attr_els[2].text + " Zi.",
+                    'address': address
+                }
+            #print entries
+            exist = False
+            for x in entries:
+                if(expose_id == x["id"]):
+                    exist = True
+                    break
+                else:
+                    continue
+            if(exist == False):
+                entries.append(details)
+
 
         self.__log__.debug('extracted: ' + str(entries))
         return entries

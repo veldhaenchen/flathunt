d16750a66a75021061e043cecde423839adda4fb
Arthur Taylor <arthur@codders.de>
2020-05-18 16:48:22 +0200

    Increased test coverage

---
+++ test/test_sender_telegram.py
@@ -0,0 +1,14 @@
+import requests_mock
+import unittest
+from flathunter.sender_telegram import SenderTelegram 
+
+class SenderTelegramTest(unittest.TestCase):
+
+    def setUp(self):
+      self.sender = SenderTelegram({ "telegram": { "bot_token": "dummy_token", "receiver_ids": [ 123 ] }})
+
+    @requests_mock.Mocker()
+    def test_send_message(self, m):
+      mock_response = '{"ok":true,"result":{"message_id":456,"from":{"id":1,"is_bot":true,"first_name":"Wohnbot","username":"wohnung_search_bot"},"chat":{"id":5,"first_name":"Arthur","last_name":"Taylor","type":"private"},"date":1589813130,"text":"hello arthur"}}'
+      m.get('https://api.telegram.org/botdummy_token/sendMessage?chat_id=123&text=result', text=mock_response)
+      self.assertEqual(None, self.sender.send_msg("result"), "Expected message to be sent")

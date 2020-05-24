731a1bb50c8de17e9afef45e60e0b0c14e89ac49
Arthur Taylor <arthur@codders.de>
2020-05-24 11:46:12 +0200

    Create empty config file if none exists

--- test/test_config.py
+++ test/test_config.py
@@ -1,5 +1,7 @@
 import unittest
 import tempfile
+import os.path
+import os
 from flathunter.config import Config 
 
 class ConfigTest(unittest.TestCase):
@@ -10,8 +12,17 @@ urls:
     """
 
     def test_loads_config(self):
+        created = False
+        if not os.path.isfile("config.yaml"):
+            config_file = open("config.yaml", "w")
+            config_file.write(self.DUMMY_CONFIG)
+            config_file.flush()
+            config_file.close()
+            created = True
         config = Config()
         self.assertTrue(len(config.get('urls')) > 0, "Expected URLs in config file")
+        if created:
+            os.remove("config.yaml")
 
     def test_loads_config_at_file(self):
        with tempfile.NamedTemporaryFile(mode='w+') as temp:

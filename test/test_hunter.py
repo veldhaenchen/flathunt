1e1c0f89920954c9bd1aeaade28f18032d3879c9
Arthur Taylor <arthur@codders.de>
2020-06-02 12:59:45 +0200

    Added tests for filtering by title

--- test/test_hunter.py
+++ test/test_hunter.py
@@ -4,6 +4,7 @@ from flathunter.crawl_immowelt import CrawlImmowelt
 from flathunter.hunter import Hunter 
 from flathunter.config import Config
 from flathunter.idmaintainer import IdMaintainer
+from dummy_crawler import DummyCrawler
 
 class HunterTest(unittest.TestCase):
 
@@ -17,9 +18,37 @@ google_maps_api:
   enable: true
     """
 
-    def setUp(self):
-        self.hunter = Hunter(Config(string=self.DUMMY_CONFIG), [CrawlImmowelt()], IdMaintainer(":memory:"))
+    FILTER_TITLES_CONFIG = """
+urls:
+  - https://www.example.com/search/flats-in-berlin
+
+excluded_titles:
+  - "wg"
+  - "tausch"
+  - "wochenendheimfahrer"
+  - "pendler"
+  - "zwischenmiete"
+"""
 
     def test_hunt_flats(self):
-        exposes = self.hunter.hunt_flats()
+        hunter = Hunter(Config(string=self.DUMMY_CONFIG), [CrawlImmowelt()], IdMaintainer(":memory:"))
+        exposes = hunter.hunt_flats()
         self.assertTrue(len(exposes) > 0, "Expected to find exposes")
+
+    def test_invalid_config(self):
+        with self.assertRaises(Exception) as context:
+            Hunter(dict(), [CrawlImmowelt()], IdMaintainer(":memory:"))
+
+        self.assertTrue('Invalid config' in str(context.exception))
+
+    def test_filter_titles(self):
+        titlewords = [ "wg", "tausch", "flat", "ruhig", "gruen" ]
+        filteredwords = [ "wg", "tausch", "wochenendheimfahrer", "pendler", "zwischenmiete" ]
+        hunter = Hunter(Config(string=self.FILTER_TITLES_CONFIG), [DummyCrawler(titlewords)], IdMaintainer(":memory:"))
+        exposes = hunter.hunt_flats()
+        self.assertTrue(len(exposes) > 4, "Expected to find exposes")
+        unfiltered = list(filter(lambda expose: any(word in expose['title'] for word in filteredwords), exposes))
+        if len(unfiltered) > 0:
+            for expose in unfiltered:
+                print("Got expose: ", expose)
+        self.assertTrue(len(unfiltered) == 0, "Expected words to be filtered")

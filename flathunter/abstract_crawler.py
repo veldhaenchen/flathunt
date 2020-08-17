6b5ab81114ac5cf4fc765aca1ae539ec263fff6b
sourcloud <sascha.scheid@pm.me>
2020-08-17 13:57:40 +0200

    Added randomization of user agent in header

--- flathunter/abstract_crawler.py
+++ flathunter/abstract_crawler.py
@@ -1,6 +1,7 @@
 """Interface for webcrawlers. Crawler implementations should subclass this"""
 import re
 import logging
+import random
 import requests
 from bs4 import BeautifulSoup
 
@@ -10,13 +11,28 @@ class Crawler:
 
     __log__ = logging.getLogger('flathunt')
     URL_PATTERN = None
+
+    USER_AGENTS = [
+        # Chrome
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36',
+        # Edge
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.18363',
+        # Firefox
+        'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1',
+        'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:54.0) Gecko/20100101 Firefox/54.0',
+        # Safari
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/11.1.2 Safari/605.1.15'
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.5 Safari/605.1.15'
+    ]
+
     HEADERS = {
         'Connection': 'keep-alive',
         'Pragma': 'no-cache',
         'Cache-Control': 'no-cache',
         'Upgrade-Insecure-Requests': '1',
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 '
-                      'Safari/537.36',
+        'User-Agent': f'{random.choice(USER_AGENTS)}',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,'
                   'application/signed-exchange;v=b3;q=0.9',
         'Sec-Fetch-Site': 'none',
@@ -26,6 +42,9 @@ class Crawler:
         'Accept-Language': 'en-US,en;q=0.9',
     }
 
+    def rotate_user_agent(self):
+        self.HEADERS['User-Agent'] = random.choice(self.USER_AGENTS)
+
     # pylint: disable=unused-argument
     def get_page(self, search_url, page_no=None):
         """Applies a page number to a formatted search URL and fetches the exposes at that page"""
@@ -33,6 +52,7 @@ class Crawler:
 
     def get_soup_from_url(self, url):
         """Creates a Soup object from the HTML at the provided URL"""
+        self.rotate_user_agent()
         resp = requests.get(url, headers=self.HEADERS)
         if resp.status_code != 200:
             self.__log__.error("Got response (%i): %s", resp.status_code, resp.content)

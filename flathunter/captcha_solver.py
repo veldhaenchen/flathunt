1aa457bd9533fde8f7eee4226620777a5ac4178f
Omar Zeidan <o.zeidan97@gmail.com>
2022-04-08 13:07:03 +0200

    Split 2captcha functions

--- flathunter/captcha_solver.py
+++ flathunter/captcha_solver.py
@@ -11,7 +11,8 @@ def solve_geetest(api_key: str, gt: str, challenge: str, page_url: str) -> str:
         f"http://2captcha.com/in.php?key={api_key}&method=geetest" +
         f"&gt={gt}&challenge={challenge}&api_server=api.geetest.com&pageurl={page_url}"
     )
-    return __retrieve_2captcha_result(api_key, submit_url)
+    captcha_id = __submit_2captcha_request(submit_url)
+    return __retrieve_2captcha_result(api_key, captcha_id)
 
 def solve_recaptcha(api_key: str, google_site_key: str, page_url: str) -> str:
     logger.debug("Trying to solve 2captcha.")
@@ -19,19 +20,25 @@ def solve_recaptcha(api_key: str, google_site_key: str, page_url: str) -> str:
         f"http://2captcha.com/in.php?key={api_key}&method=userrecaptcha" +
         f"&googlekey={google_site_key}&pageurl={page_url}"
     )
-    return __retrieve_2captcha_result(api_key, submit_url)
+    captcha_id = __submit_2captcha_request(submit_url)
+    return __retrieve_2captcha_result(api_key, captcha_id)
 
 @backoff.on_exception(wait_gen=backoff.constant,
                       exception=requests.exceptions.RequestException,
                       max_time=100)
-def __retrieve_2captcha_result(api_key: str, submit_url: str) -> str:
+def __submit_2captcha_request(submit_url: str) -> str:
     submit_response = requests.post(submit_url)
 
     if not submit_response.text.startswith("OK"):
         raise requests.HTTPError(response=submit_response)
 
-    captcha_id = submit_response.text.split("|")[1]
+    return submit_response.text.split("|")[1]
 
+
+@backoff.on_exception(wait_gen=backoff.constant,
+                      exception=requests.exceptions.RequestException,
+                      max_time=100)
+def __retrieve_2captcha_result(api_key: str, captcha_id: str) -> str:
     retrieve_url = f"http://2captcha.com/res.php?key={api_key}&action=get&id={captcha_id}"
     while True:
         retrieve_response = requests.get(retrieve_url)

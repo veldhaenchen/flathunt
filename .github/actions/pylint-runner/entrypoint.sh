a6169fee0378fb70e9576e6326f72f2f141aa632
Alexander Roidl <alexanderroidl@gmail.com>
2022-07-04 17:48:04 +0200

    Added message for pylint-runner action when failing

--- .github/actions/pylint-runner/entrypoint.sh
+++ .github/actions/pylint-runner/entrypoint.sh
@@ -15,6 +15,7 @@ if [ $(( $pylint_runner_exit_code & 32 )) != 0 ] || # usage error
    [ $(( $pylint_runner_exit_code & 2 )) != 0 ] || # error message issued
    [ $(( $pylint_runner_exit_code & 1 )) != 0 ] # fatal message issued
 then
+  echo "Looks bad; exit code $pylint_runner_exit_code"
   exit $pylint_runner_exit_code
 fi
 

9683abb335439277ccb6b61c1312d77a1c4e805b
Alexander Roidl <alexanderroidl@gmail.com>
2022-07-04 17:41:05 +0200

    Wrote exit code check for pylint-runner action

--- .github/actions/pylint-runner/entrypoint.sh
+++ .github/actions/pylint-runner/entrypoint.sh
@@ -3,4 +3,20 @@
 set -eu
 
 pip install -r requirements.txt
-pylint_runner && echo "Looking good!"
+
+pylint_runner
+pylint_runner_exit_code=$?
+
+# https://pylint.pycqa.org/en/latest/user_guide/usage/run.html#exit-codes
+if [ $(( $pylint_runner_exit_code & 32 )) != 0 ] || # usage error
+   [ $(( $pylint_runner_exit_code & 16 )) != 0 ] || # convention message issued
+   # [ $(( $pylint_runner_exit_code & 8 )) != 0 ] || # refactor message issued -> considered okay, therefore ignored
+   # [ $(( $pylint_runner_exit_code & 4 )) != 0 ] || # warning message issued -> considered okay, therefore ignored
+   [ $(( $pylint_runner_exit_code & 2 )) != 0 ] || # error message issued
+   [ $(( $pylint_runner_exit_code & 1 )) != 0 ] # fatal message issued
+then
+  exit $pylint_runner_exit_code
+fi
+
+echo "Looking good!"
+exit 0

504df4f06de852429588a7b94fcb8e2f400c64a5
Soheil Rahmat <soheil.rahmat@zalando.de>
2022-08-30 00:15:20 +0200

    Adapt heartbeat with Notifier abstract class

--- flathunter/heartbeat.py
+++ flathunter/heartbeat.py
@@ -1,9 +1,11 @@
 """Providing heartbeat messages"""
-from flathunter.logging import logger
+from flathunter.abstract_notifier import Notifier
 from flathunter.config import Config
+from flathunter.logging import logger
 from flathunter.sender_mattermost import SenderMattermost
 from flathunter.sender_telegram import SenderTelegram
 from flathunter.sender_apprise import SenderApprise
+
 
 def interval2counter(interval):
     """Transform the string interval to sleeper counter frequencies"""
@@ -17,33 +19,35 @@ def interval2counter(interval):
         return 1008
     raise Exception("No valid heartbeat instruction received - no heartbeat messages will be sent.")
 
+
 class Heartbeat:
     """Will inform the user on regular intervals whether the bot is still alive"""
+    __notifier: Notifier
+    __interval: int
 
     def __init__(self, config, interval):
         self.config = config
         if not isinstance(self.config, Config):
             raise Exception("Invalid config for hunter - should be a 'Config' object")
-        self.notifiers = self.config.notifiers()
-        if 'mattermost' in self.notifiers:
-            self.notifier = SenderMattermost(config)
-        elif 'telegram' in self.notifiers:
-            self.notifier = SenderTelegram(config)
-        elif 'apprise' in self.notifiers:
-            self.notifier = SenderApprise(config)
-        else:
-            self.notifier = None
-        self.interval = interval2counter(interval)
+        notifiers = self.config.notifiers()
 
-    def send_heartbeat(self, counter):
+        if 'mattermost' in notifiers:
+            self.__notifier = SenderMattermost(config)
+        elif 'telegram' in notifiers:
+            self.__notifier = SenderTelegram(config)
+        elif 'apprise' in notifiers:
+            self.__notifier = SenderApprise(config)
+
+        self.__interval = interval2counter(interval)
+
+    def send_heartbeat(self, counter) -> int:
         """Send a new heartbeat message"""
-        # its time for a new heartbeat message and reset counter
-        if self.notifier is not None and self.interval is not None:
-            if counter % self.interval == 0:
-                logger.info('Sending heartbeat message.')
-                self.notifier.send_msg(
-                    'Beep Boop. This is a heartbeat message. '
-                    'Your bot is searching actively for flats.'
-                )
-                counter = 0
+        # it's time for a new heartbeat message and reset counter
+        if self.__notifier and counter % self.__interval == 0:
+            logger.info('Sending heartbeat message.')
+            self.__notifier.notify(
+                'Beep Boop. This is a heartbeat message. '
+                'Your bot is searching actively for flats.'
+            )
+            counter = 0
         return counter

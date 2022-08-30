35d1378699b959236d7da72e2c0996b33899a803
Soheil Rahmat <soheil.rahmat@zalando.de>
2022-08-30 15:39:46 +0200

    Abstractmethod wrapper for Notifier

--- flathunter/abstract_notifier.py
+++ flathunter/abstract_notifier.py
@@ -1,7 +1,9 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 
 
 class Notifier(ABC):
 
+    @abstractmethod
     def notify(self, message: str):
         """Notify users with the given message"""
+        pass

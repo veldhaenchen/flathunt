2735449a09af3f6178e6fc8e6ab28f5d14168e53
Soheil Rahmat <soheil.rahmat@zalando.de>
2022-08-30 15:40:09 +0200

    Implement Notifier in Apprise

--- test/test_sender_apprise.py
+++ test/test_sender_apprise.py
@@ -9,5 +9,5 @@ class SenderAppriseTest(unittest.TestCase):
 
     @requests_mock.Mocker()
     def test_send_no_message_if_no_receivers(self, m):
-      sender = SenderApprise({"apprise": []})
-      self.assertEqual(None, sender.send_msg("result"), "Expected no message to be sent")
+        sender = SenderApprise({"apprise": []})
+        self.assertEqual(None, sender.notify("result"), "Expected no message to be sent")

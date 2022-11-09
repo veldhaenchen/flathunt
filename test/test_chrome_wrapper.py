53225ad21c947d577e4ead8e993d2dbf84c6a612
Arthur Taylor <arthur@codders.de>
2022-11-09 23:07:53 +0100

    Handle chromium version string

---
+++ test/test_chrome_wrapper.py
@@ -0,0 +1,13 @@
+import unittest
+from unittest.mock import patch
+
+from flathunter.chrome_wrapper import get_chrome_version
+
+class ChromeWrapperTest(unittest.TestCase):
+
+    @patch("flathunter.chrome_wrapper.get_command_output")
+    def test_parse_chrome_version(self, subprocess_mock):
+        subprocess_mock.side_effect = [ None, None, None, None, 'Chromium 107.0.5304.87 built on Debian bookworm/sid, running on Debian bookworm/sid', 'Google Chrome 107.0.5304.110' ]
+        self.assertEqual(get_chrome_version(), None)
+        self.assertEqual(get_chrome_version(), "107")
+        self.assertEqual(get_chrome_version(), "107")

386c11859d6a7af75a697dead1cbafe82bd22a7e
c-thaler <bummberumm@googlemail.com>
2023-02-18 10:18:07 +0100

    fixed linting issues

--- flathunt.py
+++ flathunt.py
@@ -23,22 +23,22 @@ __email__ = "harrymcfly@protonmail.com"
 __status__ = "Production"
 
 def is_current_time_between(time_from, time_till):
+    """Returns True if current time is in the given time span."""
     if time_from == time_till:
         return False
     current_time = datetime.now().time()
     if time_from < time_till:
-        return current_time >= time_from and current_time <= time_till
-    else:
-        return current_time >= time_from or current_time <= time_till
+        return time_from <= current_time <= time_till
+    return current_time >= time_from or current_time <= time_till
 
 
-def get_diff_in_secs(a, b):
-    a_secs = (a.hour * 60 + a.minute) * 60 + a.second
-    b_secs = (b.hour * 60 + b.minute) * 60 + b.second
+def get_diff_in_secs(time_a, time_b):
+    """Convert time to seconds since midnight and return the absolute difference."""
+    a_secs = (time_a.hour * 60 + time_a.minute) * 60 + time_a.second
+    b_secs = (time_b.hour * 60 + time_b.minute) * 60 + time_b.second
     if a_secs < b_secs:
         return b_secs - a_secs
-    else:
-        return a_secs - b_secs
+    return a_secs - b_secs
 
 
 def launch_flat_hunt(config, heartbeat=None):
@@ -49,7 +49,7 @@ def launch_flat_hunt(config, heartbeat=None):
     time_till = dtime.fromisoformat(config.loop_pause_till())
 
     if is_current_time_between(time_from, time_till):
-        logger.info(f"Paused loop. Waiting till {time_till}.")
+        logger.info("Paused loop. Waiting till %s.", time_till)
         time.sleep(get_diff_in_secs(datetime.now().time(), time_till) + 1)
 
     hunter = Hunter(config, id_watch)
@@ -58,7 +58,7 @@ def launch_flat_hunt(config, heartbeat=None):
 
     while config.loop_is_active():
         if is_current_time_between(time_from, time_till):
-            logger.info(f"Paused loop. Waiting till {time_till}.")
+            logger.info("Paused loop. Waiting till %s.", time_till)
             time.sleep(get_diff_in_secs(datetime.now().time(), time_till) + 1)
 
         counter += 1
